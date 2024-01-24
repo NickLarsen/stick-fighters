295e3cbc2061807a45e6ad605481f9177997a7b3
Nick Larsen <fodylarsen@gmail.com>
2024-01-23 20:18:34 -0500

    gravity and platforms

--- game.js
+++ game.js
@@ -7,6 +7,11 @@ class Point {
         this.x = x;
         this.y = y;
     }
+
+    add({ x, y }) {
+        this.x += x;
+        this.y += y;
+    }
 }
 
 class Sprite {
@@ -20,7 +25,7 @@ class Sprite {
         this.width = width;
     }
 
-    update() {};
+    update(state) {};
     render(ctx) {};
 }
 
@@ -38,7 +43,7 @@ class Scene extends Sprite {
     }
 }
 
-class Character extends Sprite {
+class Platform extends Sprite {
     color;
 
     constructor({ position, height, width, color }) {
@@ -52,19 +57,48 @@ class Character extends Sprite {
     }
 }
 
+class Character extends Sprite {
+    color;
+    motion;
+
+    constructor({ position, height, width, color }) {
+        super({ position, height, width });
+        this.color = color;
+        this.motion = new Point({ x: 0, y: 0 });
+    }
+
+    render(ctx) {
+        ctx.fillStyle = this.color;
+        ctx.fillRect(this.position.x, this.position.y, this.width, this.height);
+    }
+
+    update(state) {
+        this.motion.y += state.gravity;
+        this.position.add(this.motion);
+
+        const overlaps = state.items.find(item => item instanceof Platform && intersects(this, item));
+        if (overlaps !== undefined) {
+            this.motion.y = 0;
+            this.position.y = overlaps.position.y - this.height;
+        }
+    }
+}
+
 function makeGlobalGameState({ width, height }) {
     return {
+        gravity: 0.75,
         items: [
             new Scene({ position: new Point({ x: 0, y: 0}), width, height, color: "black" }),
-            new Character({ position: new Point({ x: 100, y: 100}), width: 50, height: 100, color: "green" }),
-            new Character({ position: new Point({ x: 800, y: 100}), width: 50, height: 100, color: "pink" }),
+            new Platform({ position: new Point({ x: 10, y: height - 30 }), width: width - 20, height: 20, color: "cyan" }),
+            new Character({ position: new Point({ x: 100, y: 100}), width: 80, height: 150, color: "green" }),
+            new Character({ position: new Point({ x: 800, y: 100}), width: 80, height: 150, color: "pink" }),
         ]
     };
 }
 
 function updateState(state) {
     for(const item of state.items) {
-        item.update();
+        item.update(state);
     }
 }
 
@@ -72,6 +106,14 @@ function renderState({ ctx, state }) {
     for(const item of state.items) {
         item.render(ctx);
     }
+}
+
+function intersects(sprite1, sprite2) {
+    const [left, right] = sprite1.position.x <= sprite2.position.x ? [sprite1, sprite2] : [sprite2, sprite1];
+    const overlapsHorizontal = right.position.x < (left.position.x + left.width);
+    const [upper, lower] = sprite1.position.y <= sprite2.position.y ? [sprite1, sprite2] : [sprite2, sprite1];
+    const overlapsVertical = lower.position.y < (upper.position.y + upper.height);
+    return overlapsHorizontal && overlapsVertical;
 }
 
 function gameLoop({ ctx, width, height }) {
