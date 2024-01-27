72917eedba98b7b79baa7c35ad75c9b1e8c1eed6
Nick Larsen <fodylarsen@gmail.com>
2024-01-26 22:34:33 -0500

    movement

--- game.js
+++ game.js
@@ -14,6 +14,42 @@ class Point {
     }
 }
 
+class Input {
+    active = false;
+}
+class Controller {
+    mappings;
+
+    inputs = {
+        left: new Input(),
+        right: new Input(),
+        jump: new Input(),
+        duck: new Input(),
+    };
+
+    constructor(mappings) {
+        this.mappings = mappings;
+        document.addEventListener("keydown", this.handleKeyDown.bind(this));
+        document.addEventListener("keyup", this.handleKeyUp.bind(this));
+    }
+
+    handleKeyDown(event) {
+        const action = this.mappings[event.code];
+        if (action !== undefined) {
+            const input = this.inputs[action];
+            input.active = true;
+        }
+    }
+
+    handleKeyUp(event) {
+        const action = this.mappings[event.code];
+        if (action !== undefined) {
+            const input = this.inputs[action];
+            input.active = false;
+        }
+    }
+}
+
 class Sprite {
     position;
     height;
@@ -58,13 +94,14 @@ class Platform extends Sprite {
 }
 
 class Character extends Sprite {
-    color;
-    motion;
+    motion = new Point({ x: 0, y: 0 });
+    grounded = false;
 
-    constructor({ position, height, width, color }) {
+    constructor({ position, height, width, color, controller, speed }) {
         super({ position, height, width });
         this.color = color;
-        this.motion = new Point({ x: 0, y: 0 });
+        this.controller = controller;
+        this.speed = speed;
     }
 
     render(ctx) {
@@ -72,26 +109,71 @@ class Character extends Sprite {
         ctx.fillRect(this.position.x, this.position.y, this.width, this.height);
     }
 
-    update(state) {
+    _updateMotion(state) {
         this.motion.y += state.gravity;
+        const jump = this.controller.inputs.jump;
+        if (jump.active && this.grounded === true) {
+            this.motion.y -= 15;
+        }
+        this.position.add(this.motion);
+        const left = this.controller.inputs.left;
+        if (left.active) {
+            this.position.x -= this.speed;
+        }
+        const right = this.controller.inputs.right;
+        if (right.active) {
+            this.position.x += this.speed;
+        }
+    }
+
+    update(state) {
+        this._updateMotion(state);
         this.position.add(this.motion);
 
+        this.grounded = false;
         const overlaps = state.items.find(item => item instanceof Platform && intersects(this, item));
         if (overlaps !== undefined) {
             this.motion.y = 0;
             this.position.y = overlaps.position.y - this.height;
+            this.grounded = true;
         }
     }
 }
 
 function makeGlobalGameState({ width, height }) {
+    const char1Controller = new Controller({
+        "KeyA": "left",
+        "KeyD": "right",
+        "KeyS": "duck",
+        "Space": "jump",
+    });
+    const char2Controller = new Controller({
+        "KeyJ": "left",
+        "KeyL": "right",
+        "KeyK": "duck",
+        "KeyI": "jump",
+    });
     return {
         gravity: 0.75,
         items: [
             new Scene({ position: new Point({ x: 0, y: 0}), width, height, color: "black" }),
             new Platform({ position: new Point({ x: 10, y: height - 30 }), width: width - 20, height: 20, color: "cyan" }),
-            new Character({ position: new Point({ x: 100, y: 100}), width: 80, height: 150, color: "green" }),
-            new Character({ position: new Point({ x: 800, y: 100}), width: 80, height: 150, color: "pink" }),
+            new Character({ 
+                position: new Point({ x: 100, y: 100}), 
+                width: 80, 
+                height: 150, 
+                color: "green", 
+                controller: char1Controller,
+                speed: 5,
+            }),
+            new Character({ 
+                position: new Point({ x: 800, y: 100}), 
+                width: 80, 
+                height: 150, 
+                color: "pink", 
+                controller: char2Controller,
+                speed: 5,
+            }),
         ]
     };
 }
